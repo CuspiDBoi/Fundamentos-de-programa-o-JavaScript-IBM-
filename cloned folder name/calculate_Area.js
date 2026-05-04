25fecd0e0a661b0ffa2407bbe1ab09c6eb019276
Pedro Henrique Peireira Lopes <pedrophl2004@gmail.com>
2026-05-03 22:25:12 -0400

    mensagem

---
+++ cloned folder name/calculate_Area.js
@@ -0,0 +1,11 @@
+let legth;
+let width
+
+function calculateArea(){
+    legth = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+
+    let area = legth * width
+
+    document.getElementById('result').innerText = `The area of rectangle is: ${area}`
+}
